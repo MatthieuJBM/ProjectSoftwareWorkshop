c1be99c71acfb7f6966127ff29ec793b3ed3d80f
Mateusz Baryla <m.baryla@protonmail.ch>
2025-07-11 18:50:03 +0200

    feat: Data objects created.

---
+++ Data/ProjectSoftwareProductionDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjectSoftwareWorkshop.Data;
+
+public class ProjectSoftwareWorkshopDbContext : DbContext
+{
+    public ProjectSoftwareWorkshopDbContext(DbContextOptions options) : base(options)
+    {
+    }
+    
+    public DbSet<Category> Kategories { get; set; }
+    public DbSet<Shop> Shops { get; set; }
+    public DbSet<Purchase> Purchases { get; set; }
+    
+}
