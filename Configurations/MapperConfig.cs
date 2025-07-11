bfe9f773fe1eeb668bdd4462e97c028a81cdacb9
Mateusz Baryla <m.baryla@protonmail.ch>
2025-07-11 18:57:50 +0200

    feat: MapperConfig implemented.

---
+++ Configurations/MapperConfig.cs
@@ -0,0 +1,21 @@
+using AutoMapper;
+using ProjectSoftwareWorkshop.Data;
+using ProjectSoftwareWorkshop.Models.Category;
+using ProjectSoftwareWorkshop.Models.Purchase;
+using ProjectSoftwareWorkshop.Models.Shop;
+
+namespace ProjectSoftwareWorkshop.Configurations;
+
+public class MapperConfig : Profile
+{
+    public MapperConfig()
+    {
+        // CreateMap<OneType, AnotherType>().ReverseMap();
+        
+        CreateMap<Category, CategoryDto>().ReverseMap();
+        
+        CreateMap<Shop, ShopDto>().ReverseMap();
+
+        CreateMap<Purchase, PurchaseDto>().ReverseMap();
+    }
+}
